fe5c045efd8fbe29b6704a2ba9fed4ef45a1f1b9
GeorgeNabilBolas <george45216899@gmail.com>
2023-07-03 10:39:35 +0300

    added splash with navigation

--- lib/features/splash/presentation/view/splash_view.dart
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -1,22 +1,48 @@
 import 'package:flutter/material.dart';
 
+import '../../../home page/presentation/view/home_page.dart';
+import 'widget/splash_animation.dart';
+import 'widget/splash_primary_widget.dart';
+import 'widget/splash_secondary_widget.dart';
+
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('KEEP IT.', style: Theme.of(context).textTheme.titleLarge),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Flexible(
+            fit: FlexFit.tight,
+            child: SplashPrimaryWidget(),
+          ),
+          Flexible(
+            child: SplashAnimation(
+              child: const SplashSecondaryWidget(),
+              onEnd: () {
+                return Future.delayed(const Duration(milliseconds: 500), () {
+                  Navigator.of(context).push(buildRoute());
+                });
+              },
+            ),
+          )
+        ],
       ),
-      bottomSheet: Padding(
-        padding: const EdgeInsets.only(bottom: 10.0),
-        child: Text(
-          'BY\nGEORGE NABIL',
-          style: Theme.of(context).textTheme.titleSmall,
-          textAlign: TextAlign.center,
-        ),
-      ),
+    );
+  }
+
+  PageRouteBuilder<dynamic> buildRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (_, __, ___) => const HomePage(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        final Tween<double> tween = Tween(begin: 0, end: 1);
+        return FadeTransition(
+          opacity: animation.drive(tween),
+          child: child,
+        );
+      },
     );
   }
 }
